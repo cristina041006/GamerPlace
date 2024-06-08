471d9aca7d12ec04740e8ef06550662c3d54eddf
Cristina <84182326+cristina041006@users.noreply.github.com>
2024-06-08 20:09:14 +0200

    Update favorite.service.ts

--- src/app/services/favorite.service.ts
+++ src/app/services/favorite.service.ts
@@ -10,7 +10,7 @@ import { Videogame } from '../interfaces/videogames';
 export class FavoriteService {
 /**Servicio de favoritos */
 
-private baseUrl : string= "http://localhost:8080";
+private baseUrl : string= "https://proyectoapi-cristina041006.onrender.com";
 constructor(private http : HttpClient) { }
 
 /**

4291217db44e400395893a3c237a26f073b457af
Cristina <Cristina04100@gmail.com>
2024-06-11 21:04:13 +0200

    Ter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { NabvarComponent } from './shared/nabvar/nabvar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { NgxUiLoaderModule } from 'ngx-ui-loader';
+import { FloatingCircleComponent } from './shared/floating-circle/floating-circle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NabvarComponent, FooterComponent, MenuComponent, NgxUiLoaderModule],
+  imports: [CommonModule, RouterOutlet, NabvarComponent, FooterComponent, MenuComponent, NgxUiLoaderModule, FloatingCircleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
